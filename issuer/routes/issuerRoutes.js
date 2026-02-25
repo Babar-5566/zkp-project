0f7c56c14cbcdd877b7fda382143e0f0adfd0090
Sk Babar Ali <skbabarali5566@gmail.com>
2026-02-25 13:56:53 +0530

    Delete issuer directory
    Why was the issuer folder present in the root justin????

--- issuer/routes/issuerRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { issueCredential } = require("../controllers/issuerController");
-
-router.post("/issue", issueCredential);
-
-module.exports = router;
